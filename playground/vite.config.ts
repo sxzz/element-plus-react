2dc401f961214bac95c0bd509a09605ba890e033
Kevin <sxzz@sxzz.moe>
2022-02-20 13:00:41 +0800

    feat(playground): add inspect plugin

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,14 +1,22 @@
-import { getTsConfigPaths } from '@element-plus/build-utils'
+import path from 'path'
+import { getTsConfigPaths, getWorkspaceRoot } from '@element-plus/build-utils'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import TsconfigPathsPlugin from '@esbuild-plugins/tsconfig-paths'
 import { tsConfigPaths } from 'vite-plugin-tsconfig-paths'
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig(async () => {
   const paths = await getTsConfigPaths()
 
   return {
-    plugins: [react(), tsConfigPaths()],
+    plugins: [
+      react(),
+      tsConfigPaths({
+        tsConfigPath: path.resolve(await getWorkspaceRoot(), 'tsconfig.json'),
+      }),
+      Inspect(),
+    ],
     optimizeDeps: {
       esbuildOptions: {
         plugins: [
