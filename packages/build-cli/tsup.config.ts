83ca87bdd3b75b4289a8f4a7d948c1ae8b254554
三咲智子 <sxzz@sxzz.moe>
2022-03-14 06:21:09 +0800

    refactor: improve build toolchain

--- packages/build-cli/tsup.config.ts
+++ packages/build-cli/tsup.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'tsup'
 
+const isDev = process.env.MODE === 'dev'
+
 export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
+  watch: isDev ? ['./src', '../build-utils'] : false,
+  noExternal: isDev ? ['@element-plus/build-utils'] : [],
 })
